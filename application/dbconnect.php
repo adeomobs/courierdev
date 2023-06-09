ed8596c328ae6e09b39a7a8e8c300820ee3f65b9
adeomobs <adeomobs@hotmail.com>
2023-06-09 10:10:30 +0100

    Removed the username and password to connect to DB from the DB file

--- application/dbconnect.php
+++ application/dbconnect.php
@@ -1,5 +1,3 @@
 <?php
-/*$mysqli = new mysqli('localhost', 'camelotc_user', 'CamelotcourieR123.', 'camelotc_track');*/
-
-$db = mysqli_connect('localhost', 'deluxcou_user', 'DeluxcourieR123.', 'deluxcou_track');
+$db = mysqli_connect('localhost', 'user', 'password', 'DB');
 ?>
